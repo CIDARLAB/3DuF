d4a63743291d73e2937a07f4be7225e479bba045
Aaron Heuckroth <a.heuckroth@gmail.com>
2015-07-20 18:40:05 -0400

    started building data model, added unit tests

---
+++ src/app/device.js
@@ -0,0 +1,108 @@
+var values = require('./values.js');
+var uuid = require('node-uuid');
+
+/* The Device stores information about the physical properties of a given design. */
+class Device {
+	constructor(width, height, name = "New Device"){
+		this.defaults = {};
+		this.layers = [];
+		this.groups = [];
+		this.params = {};
+		this.params.name = new values.StringValue(name);
+		this.params.width = new values.FloatValue(width);
+		this.params.height = new values.FloatValue(height);
+	}
+
+	/* Sort the layers such that they are ordered from lowest to highest z_offset. */
+	sortLayers(){
+		this.layers.sort(function(a, b){
+			return a.params.z_offset.value - b.params.z_offset.value;
+		});
+	}
+
+	/* Add a layer, and re-sort the layers array.*/
+	addLayer(layer){
+		this.layers.push(layer);
+		this.sortLayers();
+	}
+}
+
+class Layer {
+	constructor(z_offset, flip, name = "New Layer"){
+		this.params = {};
+		if (z_offset <0 ){
+			throw new Error("Layer z_offset must be >= 0.");
+		}
+		this.params.z_offset = new values.FloatValue(z_offset);
+		this.params.flip = new values.BooleanValue(flip);
+		this.params.name = new values.StringValue(name);
+		this.features = {};
+		this.featureCount = 0;
+	}
+
+	addFeature(feature){
+		this.__ensureIsAFeature(feature);
+		this.features[feature.id] = feature;
+		this.featureCount += 1;
+	}
+
+	__ensureIsAFeature(feature){
+		if (!(feature instanceof Feature)) throw new Error("Provided value is not a Feature! Did you pass an ID by mistake?");
+	}
+
+	__ensureFeatureExists(feature){
+		if (!this.containsFeature()) throw new Error("Layer does not contain the specified feature!");
+	}
+
+	__ensureFeatureIDExists(featureID){
+		if (!this.containsFeatureID(featureID)) throw new Error("Layer does not contain a feature with the specified ID!");
+	}
+
+	getFeature(featureID){
+		this.__ensureFeatureIDExists(featureID)
+		return this.features[featureID];
+	}
+
+	removeFeature(feature){
+		this.__ensureFeatureExists(feature);
+		this.features[feature.id] = undefined;
+		this.featureCount -= 1;
+	}
+
+	containsFeature(feature){
+		this.__ensureIsAFeature(feature);
+		return (this.features.hasOwnProperty(feature.id));
+	}
+
+	containsFeatureID(featureID){
+		return this.features.hasOwnProperty(featureID);
+	}
+}
+
+class Group {
+	constructor(){
+		this.name = "";
+		this.parent = null;
+	}
+}
+
+class Feature {
+	constructor(type, name = "New Feature"){
+		this.id = this.generateID();
+		this.name = name;
+		this.connections = {};
+		this.type = "";
+		this.params = {};
+		this.group = null;
+	}
+
+	generateID(){
+		return uuid.v1();
+	}
+}
+
+exports.Device = Device;
+exports.Layer = Layer;
+exports.Group = Group;
+exports.Feature = Feature;
+
--- src/test/values_test.js
+++ src/test/values_test.js
@@ -1,9 +1,10 @@
 var should = require("should");
 var Values = require("../app/values.js");
 var FloatValue = Values.FloatValue;
-var RelativeValue = Values.RelativeValue;
 var IntegerValue = Values.IntegerValue;
 var PointValue = Values.PointValue;
+var BooleanValue = Values.BooleanValue;
+var StringValue = Values.StringValue;
 
 describe('Values', function(){
 	describe('#FloatValue', function(){
@@ -68,6 +69,59 @@ describe('Values', function(){
 		});
 		it("should not allow a malformed point [1,3,4]", function(){
 			(function() {new PointValue([1,3,4])}).should.throwError();
+		});
+	});
+	describe("#BooleanValue", function(){
+		it("should allow a value of true", function(){
+			let val = new BooleanValue(true);
+			val.type.should.equal("Boolean");
+			val.value.should.equal(true);
+		});
+		it("Should allow a value of false", function(){
+			let val = new BooleanValue(false);
+			val.type.should.equal("Boolean");
+			val.value.should.equal(false);
+		});
+		it("should not allow the point [0,1]", function(){
+			(function() {new BooleanValue([0,1])}).should.throwError();
+		});
+		it("should not allow a value of 1.1", function(){
+			(function() {new BooleanValue(1.1)}).should.throwError();
+		});
+		it("should not allow a value of 1", function(){
+			(function() {new BooleanValue(1)}).should.throwError();
+		});
+		it("should not allow a value of 'foobar'", function(){
+			(function() {new BooleanValue('foobar')}).should.throwError();
+		});
+		it("should not allow a malformed point [1,3,4]", function(){
+			(function() {new BooleanValue([1,3,4])}).should.throwError();
+		});
+	});
+
+	describe("#StringValue", function(){
+		it("should allow a value of 'foobar'", function(){
+			let val = new StringValue("foobar");
+			val.type.should.equal("String");
+			val.value.should.equal("foobar");
+		});
+		it("should not allow a value of true", function(){
+			(function() {new StringValue(true)}).should.throwError();
+		});
+		it("Should not allow a value of false", function(){
+			(function() {new StringValue(false)}).should.throwError();
+		});
+		it("should not allow the point [0,1]", function(){
+			(function() {new StringValue([0,1])}).should.throwError();
+		});
+		it("should not allow a value of 1.1", function(){
+			(function() {new StringValue(1.1)}).should.throwError();
+		});
+		it("should not allow a value of 1", function(){
+			(function() {new StringValue(1)}).should.throwError();
+		});
+		it("should not allow a malformed point [1,3,4]", function(){
+			(function() {new StringValue([1,3,4])}).should.throwError();
 		});
 	});
 });
