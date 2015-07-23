4ee9a22b14cb4f18d24c8e0d7b71a6c3b1ae8f1e
Aaron Heuckroth <a.heuckroth@gmail.com>
2015-07-23 16:39:18 -0400

    updated old example python files to use array colors

--- old/python_examples/transposer_tiny.py
+++ old/python_examples/transposer_tiny.py
@@ -2,8 +2,8 @@ import microfluidic_SCAD_generator
 
 ufgen = microfluidic_SCAD_generator.UF_Generator("transposer_tiny")
 
-c = ufgen.create_layer(ufgen.layer_offset, "c", True, color="Red")
-f = ufgen.create_layer(0, "f", color="Blue")
+c = ufgen.create_layer(ufgen.layer_offset, "c", True, color=[1,0,0])
+f = ufgen.create_layer(0, "f", color=[0,0,1])
 
 x_offset_start = 30
 y_offset_start = 20
