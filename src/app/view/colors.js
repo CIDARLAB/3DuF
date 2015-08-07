5f79b68b3b7991def5b0e44c018d700251bf68c0
Aaron Heuckroth <a.heuckroth@gmail.com>
2015-08-07 02:09:01 -0400

    layer coloring, alpha
    Still haven't figured out how to get the layers to rank correctly so
    current is on top.
    Re-think how current displayed layer works. Different from current
    working layer?

--- src/app/view/colors.js
+++ src/app/view/colors.js
@@ -1,19 +1,116 @@
-var ColorUtils = require("../utils/colorUtils");
-var hexStringToPaperColor = ColorUtils.hexStringToPaperColor;
 
 //Colors taken from: http://www.google.ch/design/spec/style/color.html
-module.exports.RED_500 = hexStringToPaperColor("#F44336");
-module.exports.INDIGO_500 = hexStringToPaperColor("#3F51B5");
-module.exports.GREEN_500 = hexStringToPaperColor("#4CAF50");
-module.exports.DEEP_PURPLE_500 = hexStringToPaperColor("#673AB7");
-module.exports.BLUE_50 = hexStringToPaperColor("#e3f2fd");
-module.exports.BLUE_100 = hexStringToPaperColor("#BBDEFB");
-module.exports.BLUE_300 = hexStringToPaperColor("#64B5F6");
-module.exports.BLUE_500 = hexStringToPaperColor("#2196F3");
-module.exports.GREY_700 = hexStringToPaperColor("#616161");
-module.exports.GREY_500 = hexStringToPaperColor("#9E9E9E");
-module.exports.AMBER_50 = hexStringToPaperColor("#FFF8E1");
-module.exports.PINK_500 = hexStringToPaperColor("#E91E63");
-module.exports.PINK_300 = hexStringToPaperColor("#F06292");
-module.exports.BLACK = hexStringToPaperColor("#000000");
-module.exports.WHITE = hexStringToPaperColor("#FFFFFF");
+module.exports.RED_500 = "#F44336";
+module.exports.INDIGO_500 = "#3F51B5";
+module.exports.GREEN_500 = "#4CAF50";
+module.exports.DEEP_PURPLE_500 = "#673AB7";
+module.exports.BLUE_50 = "#e3f2fd";
+module.exports.BLUE_100 = "#BBDEFB";
+module.exports.BLUE_300 = "#64B5F6";
+module.exports.BLUE_500 = "#2196F3";
+module.exports.GREY_200 = "#EEEEEE";
+module.exports.GREY_700 = "#616161";
+module.exports.GREY_500 = "#9E9E9E";
+module.exports.AMBER_50 = "#FFF8E1";
+module.exports.PINK_500 = "#E91E63";
+module.exports.PINK_300 = "#F06292";
+module.exports.BLACK = "#000000";
+module.exports.WHITE = "#FFFFFF";
+
+var defaultColorKeys = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900"]
+var darkColorKeys = ["300", "400", "500", "600", "700", "800", "900"]
+
+var indigo = {
+	"900": "#" + "1A237E",
+	"800": "#" + "283593",
+	"700": "#" + "303F9F",
+	"600": "#" + "3949AB",
+	"500": "#" + "3F51B5",
+	"400": "#" + "5C6BC0",
+	"300": "#" + "7986CB",
+	"200": "#" + "9FA8DA",
+	"100": "#" + "C5CAE9",
+	"50": "#" + "E8EAF6",
+	"A100": "#" + "8C9EFF",
+	"A200": "#" + "536DFE",
+	"A400": "#" + "3D5AFE",
+	"A700": "#" + "304FFE",
+}
+
+var red = {
+	"900": "#" + "B71C1C",
+	"800": "#" + "C62828",
+	"700": "#" + "D32F2F",
+	"600": "#" + "E53935",
+	"500": "#" + "F44336",
+	"400": "#" + "EF5350",
+	"300": "#" + "E57373",
+	"200": "#" + "EF9A9A",
+	"100": "#" + "FFCDD2",
+	"50": "#" + "FFEBEE",
+	"A100": "#" + "FF8A80",
+	"A200": "#" + "FF5252",
+	"A400": "#" + "FF1744",
+	"A700": "#" + "D50000"
+}
+
+var layerColors = {
+	"indigo": indigo,
+	"red": red
+}
+
+var decimalToIndex = function(decimal, indices){
+	return Math.round((indices-1) * decimal);
+}
+
+var decimalToLayerColor = function(decimal, layerColors, orderedKeys){
+	let index = decimalToIndex(decimal, orderedKeys.length);
+	let key = orderedKeys[index];
+	return layerColors[key];
+}
+
+var renderAllColors = function(layer, orderedKeys){
+	for (let i =0 ; i < orderedKeys.length; i ++){
+		
+		new paper.Path.Circle({
+			position: new paper.Point(0+(i*1000), 0),
+			fillColor: layer[orderedKeys[i]],
+			radius: 500
+		});
+	}
+
+	for (let i = 0; i < orderedKeys.length; i ++){
+		let color = decimalToLayerColor(i/orderedKeys.length, layer, orderedKeys);
+		new paper.Path.Circle({
+			position: new paper.Point(0+(i*1000), 2000),
+			fillColor: layer[orderedKeys[i]],
+			radius: 500
+		});
+	}
+}
+
+var getLayerColors = function(layer){
+	if (layer.color) return layerColors[layer.color];
+	else return layerColors["red"]
+}
+
+var getDefaultLayerColor = function(layer){
+	return getLayerColors(layer)["500"];
+}
+
+var getDefaultFeatureColor = function(featureClass, layer){
+	let height = featureClass.getDefaultValues()["height"];
+	let decimal = height / layer.estimateLayerHeight();
+	if (!layer.flip) decimal = 1-decimal;
+	let colors = getLayerColors(layer);
+	return decimalToLayerColor(decimal, colors, darkColorKeys);
+}
+
+module.exports.getDefaultLayerColor = getDefaultLayerColor;
+module.exports.getDefaultFeatureColor = getDefaultFeatureColor;
+module.exports.getLayerColors = getLayerColors;
+module.exports.decimalToLayerColor = decimalToLayerColor;
+module.exports.defaultColorKeys = defaultColorKeys;
+module.exports.darkColorKeys = darkColorKeys;
+module.exports.layerColors = layerColors;
+module.exports.renderAllColors = renderAllColors;
--- src/app/view/deviceRenderer.js
+++ src/app/view/deviceRenderer.js
@@ -3,17 +3,26 @@ var DEFAULT_STROKE_COLOR = Colors.GREY_700;
 var BORDER_THICKNESS = 5; // pixels
 
 function renderDevice(device, strokeColor = DEFAULT_STROKE_COLOR) {
+    let background = new paper.Path.Rectangle({
+        from: paper.view.bounds.topLeft,
+        to: paper.view.bounds.bottomRight,
+        fillColor: Colors.GREY_200,
+        strokeColor: null
+    })
     let thickness = BORDER_THICKNESS / paper.view.zoom;
     let width = device.params.getValue("width");
     let height = device.params.getValue("height");
     let border = new paper.Path.Rectangle({
         from: new paper.Point(0, 0),
         to: new paper.Point(width, height),
-        fillColor: null,
+        fillColor: Colors.WHITE,
         strokeColor: strokeColor,
         strokeWidth: thickness
     });
-    return border;
+
+    let group = new paper.Group([background, border]);
+
+    return group;
 }
 
 module.exports.renderDevice = renderDevice;
