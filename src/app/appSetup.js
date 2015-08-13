291ea7962c6aec6bcd18fb42895610f802504fe0
Aaron Heuckroth <a.heuckroth@gmail.com>
2015-08-13 00:09:06 -0400

    moved outdated source files to old/ directory. removed graphics folder entirely

--- src/app/appSetup.js
+++ src/app/appSetup.js
@@ -1,5 +1,3 @@
-var CanvasManager = require("./graphics/CanvasManager");
-//var CanvasManager = require("./graphics/CanvasManager");
 var Registry = require("./core/registry");
 var Device = require('./core/device');
 var Layer = require('./core/layer');
@@ -11,7 +9,6 @@ var Colors = require("./view/colors");
 var ThreeDeviceRenderer = require("./view/render3D/ThreeDeviceRenderer");
 var Examples = require("./examples/jsonExamples");
 
-var manager;
 var view;
 var viewManager;
 var grid;
@@ -19,7 +16,6 @@ var grid;
 paper.setup("c");
 
 window.onload = function() {
-    manager = new CanvasManager(document.getElementById("c"));
     view = new PaperView(document.getElementById("c"));
     viewManager = new ViewManager(view);
     grid = new AdaptiveGrid();
@@ -28,30 +24,11 @@ window.onload = function() {
 
     Registry.viewManager = viewManager;
 
-    /*
-    if (!localStorage){
-        manager.loadDeviceFromJSON(JSON.parse(Examples.example1));
-    }
-    else if (!localStorage.getItem('currentDevice')) {
-        localStorage.setItem('currentDevice', Examples.example1);
-    } else {
-        try {
-            manager.loadFromStorage();
-        } catch (err) {
-            localStorage.setItem('currentDevice', Examples.example1);
-            manager.loadFromStorage();
-        }
-    }
-    */
-
-    manager.loadDeviceFromJSON(JSON.parse(Examples.example1));
-
-
+    viewManager.loadDeviceFromJSON(JSON.parse(Examples.example1));
     viewManager.updateGrid();
     Registry.currentDevice.updateView();
 
     window.dev = Registry.currentDevice;
-    window.man = manager;
     window.Registry = Registry;
 
     window.view = Registry.viewManager.view;
