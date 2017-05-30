ec7fb8f4dd2c50da677e9a8120fb6828b65d8e5e
JLippai <jlippai@bu.edu>
2017-05-29 23:04:53 -0400

    Cleaned up channel param window

--- 3DuFApp.js
+++ 3DuFApp.js
@@ -32228,8 +32228,7 @@ let basicFeatures = {
         },
         heritable: {
             "channelWidth": "Float",
-            "height": "Float",
-            "width": "Float"
+            "height": "Float"
         },
         defaults: {
             "channelWidth": .75 * 1000,
