5da209bd25a40c3c1a156b812ee48534b9cc2ec2
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2017-11-13 05:46:11 -0500

    Updated the bug in tree generation code where spacing wasn't correct because the spacings were measured from the centers

--- 3DuFApp.js
+++ 3DuFApp.js
@@ -37393,6 +37393,7 @@ function generateTwig(treepath, px, py, cw, stagelength, newspacing, level, maxl
 
     if (level == maxlevel) {
         islast = true;
+        console.log("Final Spacing: " + newspacing);
     }
 
     drawtwig(treepath, px, py, cw, stagelength, newspacing, islast);
@@ -37425,7 +37426,7 @@ var TreeTarget = function (params) {
     } else {
         isodd = true;
     }
-    let w = leafs * cw + spacing * (leafs - 1);
+    let w = spacing * (leafs / 2 + 1);
     let l = (levels + 1) * stagelength;
 
     console.log("CW: " + cw + " levels: " + levels + " width: " + w + " length: " + l);
