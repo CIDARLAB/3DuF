b8df5b3281fea0b9327fad70d8a10a011c6c6871
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2018-12-23 23:40:20 -0500

    Updated and moved the browser detection

--- src/app/appSetup.js
+++ src/app/appSetup.js
@@ -16,23 +16,16 @@ paper.setup("c");
 
 function getQueryVariable(variable)
 {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-        var pair = vars[i].split("=");
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
+    for (let i=0;i<vars.length;i++) {
+        let pair = vars[i].split("=");
         if(pair[0] == variable){return pair[1];}
     }
     return(false);
 }
 
 window.onload = function() {
-
-    let isChrome = !!window.chrome && !!window.chrome.webstore;
-
-    if(!isChrome){
-        alert("Warning ! Unsupported browser detected. 3DuF has been developed and tested only in Chrome. " +
-            "The tool may not work correctly on this browser");
-    }
 
     let view = new PaperView(document.getElementById("c"));
     viewManager = new ViewManager(view);
