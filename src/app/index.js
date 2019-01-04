f0c8954004d688f2aeb875e30657b826579506fa
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2019-01-03 23:51:47 -0500

    Created an index.js to become a library

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+const ViewManager = require('view/viewManager');
+const AdaptiveGrid = require('view/grid/adaptiveGrid');
+const PaperView = require('view/paperView');
+const Registry = require('core/registry');
+
+module.exports.ViewManager = ViewManager;
+module.exports.AdaptiveGrid = AdaptiveGrid;
+module.exports.PaperView = PaperView;
+module.exports.Registry = Registry;
