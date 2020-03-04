4b8c9e374408df416372a7610a9cb14bd4db794f
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-03-03 22:53:04 -0500

    Well apparently I didn't merge this well enough

--- src/app/appSetup.js
+++ src/app/appSetup.js
@@ -24,33 +24,7 @@ window.onload = function() {
         window.view = Registry.viewManager.view;
 
         // Registry.threeRenderer = new ThreeDeviceRenderer(document.getElementById("renderContainer"));
+        Registry.viewManager.setupToolBars();
+        Registry.viewManager.generateBorder();
+    };
 
-        if (false != getQueryVariable("file")) {
-            let url = decodeURIComponent(getQueryVariable("file"));
-            //Download the json
-            fetch(url) // Call the fetch function passing the url of the API as a parameter
-                .then(resp => resp.json())
-                .then(function(data) {
-                    // Create and append the li's to the ul
-                    //alert(data);
-                    console.log(data);
-                    viewManager.loadDeviceFromJSON(data);
-                    viewManager.updateGrid();
-                    Registry.currentDevice.updateView();
-
-                    window.dev = Registry.currentDevice;
-                    window.Registry = Registry;
-
-    Registry.viewManager = viewManager;
-
-    viewManager.loadDeviceFromJSON(JSON.parse(Examples.example2));
-    viewManager.updateGrid();
-    Registry.currentDevice.updateView();
-
-    window.dev = Registry.currentDevice;
-    window.Registry = Registry;
-
-    window.view = Registry.viewManager.view;
-    // Registry.threeRenderer = new ThreeDeviceRenderer(document.getElementById("renderContainer"));
-    Registry.viewManager.setupToolBars();
-};
