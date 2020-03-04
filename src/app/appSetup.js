6329e8df2c3a872128e4fe6d61ad5e9780a930a3
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-03-03 22:55:54 -0500

    Small Updates to keep the semblance of things working correctly

--- src/app/appSetup.js
+++ src/app/appSetup.js
@@ -23,6 +23,5 @@ window.onload = function() {
 
         // Registry.threeRenderer = new ThreeDeviceRenderer(document.getElementById("renderContainer"));
         Registry.viewManager.setupToolBars();
-        Registry.viewManager.generateBorder();
     };
 
