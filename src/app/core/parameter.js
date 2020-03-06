b36160880c3359ebf9e7a35e3690fb774f6404ef
Sherman Rose <sherman@shermanrose.uk>
2020-03-06 01:34:27 +0000

    Convert registry to class for ES6 compliance

--- src/app/core/parameter.js
+++ src/app/core/parameter.js
@@ -83,6 +83,7 @@ export default class Parameter {
      * @returns {void}
      */
     static registerParamType(typeString, isValid, description) {
+      console.log(Registry)
         Registry.registeredParams[typeString] = {
             isValid: isValid,
             description: description
