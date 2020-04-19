516a01b27e76846cbb1a75b9a66c4c8f8b29fc0b
AS JK <sjain70@gmail.com>
2020-04-18 17:14:11 -0700

    added thermocycler ports

--- thermoCycler.js
+++ thermoCycler.js
@@ -1,5 +1,6 @@
 import Template from "./template";
 import paper from "paper";
+import ComponentPort from "../core/componentPort";
 
 export default class thermoCycler extends Template{
   constructor(){
@@ -8,56 +9,60 @@ export default class thermoCycler extends Template{
 
   __setupDefinitions() {
     this.__unique = {
-      position : "Rec"
+      position : "Point"
     };
     this.__heritable = {
-      orientation : "String",
+      rotation : "Float",
       width : "Float",
       length : "Float",
       height : "Float",
       temperature : "Float"
     };
     this.__defaults = {
-      orientation : "V",
+      rotation : 0,
       width : 20 * 1000,
       length : 40 * 1000,
       height : 5 * 1000,
       temperature : 0.03 * 1000
     };
     this.__units = {
-      orentiation : "",
+      rotation : "&deg",
       width : "&mu;m",
       length : "&mu;m",
       height : "&mu;m",
       temperature : "°C"
     };
     this.__minimum = {
-      orientation : "H",
+      rotation : 0,
       width : 10 * 1000,
       length : 10 * 1000,
       height : 1.25 * 1000,
       temperature : 0 * 1000
     };
     this.__maximum = {
-      orientation : "H",
+      rotation : 90,
       width : 60 * 1000,
       length : 60 * 1000,
       height : 10 * 1000,
       temperature : 0.1 * 1000
     };
-    this.__placementTool = "componentPositionTool";
+    this.__placementTool = "multilayerPositionTool";
     this.__toolParams = {
       position : "position"
     };
 
     this.__featureParams = {
       position : "position",
-      orientation : "orientation",
+      rotation : "rotation",
+      length: "length",
+      width: "width",
       temperature : "temperature"
     };
 
     this.__targetParams = {
-      orientation : "orientation",
+      rotation : "rotation",
+      length: "length",
+      width: "width",
       temperature : "temperature"
     };
 
@@ -68,21 +73,42 @@ export default class thermoCycler extends Template{
 
   render2D(params, key) {
     let position = params["position"];
-    let orientation = params["orientation"];
-    let width = params["width"];
-    let length = params["length"];
-    let height = params["height"];
-    let temperature = params["temperature"];
+    let rotation = params["rotation"];
+    let px = position[0];
+    let py = position[1];
+    let l = params["length"];
+    let w = params["width"];
     let color = params["color"];
-    let x = params["position"][0];
-    let y = params["position"][1];   
-    let rect = paper.Path.Rectangle({
-      size: [vec.length, width],
-      point: start,
-      fillColor: color,
-      strokeWidth: 2
-    }); 
-    return rect;
+    let startX = px - w / 2;
+    let startY = py - l / 2;
+    let endX = px + w / 2;
+    let endY = py + l / 2;
+    let startPoint = new paper.Point(startX, startY);
+    let endPoint = new paper.Point(endX, endY);
+    let rendered = new paper.CompoundPath();
+    let cirrad = l / 4;
+    let centerr = new paper.Point(px - w,py - l);
+
+    let rec = paper.Path.Rectangle({
+        from: startPoint,
+        to: endPoint,
+        radius: 0,
+        fillColor: color,
+        strokeWidth: 0
+    });
+    rendered.addChild(rec);
+
+//    let cir = new paper.Path.Circle({
+//      center: centerr,
+//      radius: cirrad,
+//      fillColor: 'black'
+//    })
+//    rendered.addChild(cir);
+
+    rendered.fillColor = color;
+//    cir.fillColor = 'black';
+//    rec.addChild(cir)
+    return rendered.rotate(rotation,px,py);
   }
 
   render2DTarget(key, params){
@@ -96,14 +122,9 @@ export default class thermoCycler extends Template{
 
     let ports = [];
 
-    ports.push(new ComponentPort(0, -l / 2, "1", "FLOW"));
-
-    ports.push(new ComponentPort(w / 2, 0, "2", "FLOW"));
-
-    ports.push(new ComponentPort(0, l / 2, "3", "FLOW"));
-
-    ports.push(new ComponentPort(-w / 2, 0, "4", "FLOW"));
-
+    ports.push(new ComponentPort(-w / 2, -l / 6, "1", "CONTROL"));
+    ports.push(new ComponentPort(-w / 2, l / 6, "2", "CONTROL"));
+    
     return ports;
   }
 }
