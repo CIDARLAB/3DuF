a7af8dfceb140a5f0cf4f3bf4bd181ec3a92f9dd
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-11-16 14:40:20 -0500

    Updated to work with the new layer constructor

--- src/app/core/layer.js
+++ src/app/core/layer.js
@@ -6,13 +6,28 @@ import Params from "./params";
 import * as Registry from "./registry";
 
 export default class Layer {
-    constructor(values, name = "New Layer") {
+    constructor(values, name = "New Layer", type = "FLOW", group = "0") {
         this.params = new Params(values, Layer.getUniqueParameters(), Layer.getHeritableParameters());
         this.name = String(name);
         this.features = {};
         this.featureCount = 0;
         this.device = undefined;
         this.color = undefined;
+        this.id = Layer.generateID();
+        this.__type = type;
+        this.group = group;
+    }
+
+    get type(){
+        return this.__type;
+    }
+
+    /**
+     * Generates a random id
+     * @returns {String} Random ID string
+     */
+    static generateID() {
+        return Registry.generateID();
     }
 
     addFeature(feature) {
@@ -192,7 +207,7 @@ export default class Layer {
         if (!json.hasOwnProperty("features")) {
             throw new Error("JSON layer has no features!");
         }
-        let newLayer = new Layer(json.params, json.name);
+        let newLayer = new Layer(json.params, json.name, json.type, json.group);
         newLayer.__loadFeaturesFromInterchangeV1(json.features);
         if (json.color) newLayer.color = json.color; //TODO: Figure out if this needs to change in the future
         return newLayer;
