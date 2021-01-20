9a167c9118991b75b2ba274751ba22d1f9bf0a5e
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2021-01-20 16:30:28 -0500

    Managed to integrate the base visualizer

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <Visualiser />
+    </div>
+</template>
+
+<script>
+import Visualiser from "./components/Visualiser";
+
+export default {
+    name: "App",
+    components: {
+        Visualiser
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
