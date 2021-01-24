eddf8df0d1f74f636852ace632ac558dd0ef3aa3
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2021-01-23 23:03:28 -0500

    Updated the help dialog code to use vuetify. The general pattern used here will work for us to just drop the dialog in order

--- src/views/layouts/sidebar_layout.vue
+++ src/views/layouts/sidebar_layout.vue
@@ -1,58 +1,61 @@
 <template>
-  <div>
-    <v-navigation-drawer
-      app
-      permanent
-      class="pt-4"
-      color="grey lighten-3"
-    >
-        <v-img
-          class="mx-auto"
-          src="img/logo.png"
-          alt="3DuF Logo"
-          style="width:90%"
-        />
+    <div>
+        <v-navigation-drawer app permanent class="pt-4" color="grey lighten-3">
+            <v-img class="mx-auto" src="img/logo.png" alt="3DuF Logo" style="width: 90%" />
 
-      <v-divider></v-divider>
+            <v-divider></v-divider>
 
-      <v-list>
-        <v-list-item-group mandatory color="indigo">
-          <v-list-item
-            v-for="[icon, text] in buttons"
-            :key="icon"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>{{ icon }}</v-icon>
-            </v-list-item-icon>
+            <v-list>
+                <v-list-item-group mandatory color="indigo">
+                    <v-list-item v-for="[icon, text] in buttons" :key="icon" link>
+                        <v-list-item-icon>
+                            <v-icon>{{ icon }}</v-icon>
+                        </v-list-item-icon>
 
-            <v-list-item-content>
-              <v-list-item-title>{{ text }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+                        <v-list-item-content>
+                            <v-list-item-title>{{ text }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+            <HelpDialog v-model="dialog" />
+        </v-navigation-drawer>
 
-    <v-main>
-      <slot name="main"></slot>
-    </v-main>
-  </div>
+        <v-main id="visualizer-slot">
+            <slot name="main"></slot>
+        </v-main>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+#visualizer-slot {
+    width: 100%;
+    min-height: 100vh;
+}
 </style>
 
 <script>
-  export default {
-    data(){
-      return {
-      buttons: [
-        ["mdi-devices", "Edit Device"],
-        ["mdi-border-all", "Edit Border"],
-        ["mdi-toolbox", "Select Tool"],
-      ],
-      }
-    } 
-  }
+import HelpDialog from "../../components/HelpDialog.vue";
+
+export default {
+    components: {
+        HelpDialog
+    },
+    data() {
+        return {
+            dialog: false,
+            buttons: [
+                ["mdi-devices", "Edit Device"],
+                ["mdi-border-all", "Edit Border"],
+                ["mdi-toolbox", "Select Tool"]
+            ]
+        };
+    },
+    methods: {
+        showHelpDialog() {
+            alert("TEST");
+            this.$dialog.show(HelpDialog);
+        }
+    }
+};
 </script>
