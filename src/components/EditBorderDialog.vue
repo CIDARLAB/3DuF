3347ea472b51e1574682782ec668b975eeaa5914
Sherman Rose <sherman@shermanrose.uk>
2021-01-26 09:34:37 +0000

    Refacted dialogs to use a base dialog component

--- src/components/EditBorderDialog.vue
+++ src/components/EditBorderDialog.vue
@@ -1,39 +1,37 @@
 <template>
-    <v-row justify="center">
-        <v-dialog v-model="dialog" persistent max-width="600">
-            <template v-slot:activator="{ on, attrs }">
-                <div color="primary" dark v-bind="attrs" v-on="on" class="button_row">
-                    <a id="edit_border_button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised feature-button"> Edit Border</a>
-                </div>
-            </template>
-            <v-card>
-                <v-card-title class="headline"> Edit Device Border </v-card-title>
-                <v-card-text>
-                    <h4>Drag Drop the DXF file containing border to import:</h4>
-                    <div class="mdl-dialog__content">
-                        <canvas id="border_import_panel" tabindex="1" width="300" height="200"></canvas>
-                        <br />
-                        <input type="file" class="upload" id="dxf_input" />
-                    </div>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="green darken-1" text @click="dialog = false"> Import Borde </v-btn>
-                    <v-btn color="green darken-1" text @click="dialog = false"> Delete Border </v-btn>
-                    <v-btn color="green darken-1" text @click="dialog = false"> Save </v-btn>
-                    <v-btn color="green darken-1" text @click="dialog = false"> Cancel </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </v-row>
+  <Dialog title="Edit Border">
+    <template #content>
+      <h4>Drag Drop the DXF file containing border to import:</h4>
+      <div class="mdl-dialog__content">
+          <canvas id="border_import_panel" tabindex="1" width="300" height="200"></canvas>
+          <br />
+          <input type="file" class="upload" id="dxf_input" />
+      </div>
+    </template>
+    <template v-slot:actions="{ callbacks }">
+        <v-btn color="green darken-1" text @click="callbacks.close()">Import Border</v-btn>
+        <v-btn color="green darken-1" text @click="callbacks.close()">Delete Border</v-btn>
+        <v-btn color="green darken-1" text @click="callbacks.close(onSave)">Save</v-btn>
+        <v-btn color="green darken-1" text @click="callbacks.close()">Cancel</v-btn>
+    </template>
+ </Dialog>
 </template>
 
 <script>
+import Dialog from "@/components/Dialog.vue"
 export default {
+    components: {
+      Dialog
+    },
     data() {
         return {
             dialog: false
         };
+    },
+    methods: {
+      onSave() {
+        console.log("Saved data for Edit Border")
+      }
     }
 };
 </script>
