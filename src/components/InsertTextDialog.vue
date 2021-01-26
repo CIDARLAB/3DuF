ce9f59a44441e540eb3f8ebcc108c51b313d0847
Sherman Rose <sherman@shermanrose.uk>
2021-01-26 09:34:37 +0000

    Refacted dialogs to use a base dialog component

--- src/components/InsertTextDialog.vue
+++ src/components/InsertTextDialog.vue
@@ -1,40 +1,36 @@
 <template>
-    <v-dialog v-model="dialog" persistent max-width="600">
-        <template v-slot:activator="{ on, attrs }">
-            <div class="button_row">
-                <a color="primary" dark v-bind="attrs" v-on="on" id="insert_text_button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised feature-button">
-                    Insert Text</a
-                >
-            </div>
-        </template>
-        <v-card>
-            <v-card-title class="headline"> Insert Text: </v-card-title>
-            <v-card-text>
-                <form action="#">
-                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                        <input class="mdl-textfield__input" type="text" id="inserttext_textinput" />
-                        <label class="mdl-textfield__label" for="inserttext_textinput">Text</label>
-                    </div>
-                </form>
-            </v-card-text>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="green darken-1" text @click="dialog = false"> Cancel </v-btn>
-                <v-btn color="green darken-1" text @click="dialog = false"> Insert </v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-dialog>
+  <Dialog title="Insert Text">
+    <template #content>
+    <form action="#">
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input class="mdl-textfield__input" type="text" id="inserttext_textinput" />
+          <label class="mdl-textfield__label" for="inserttext_textinput">Text</label>
+        </div>
+      </form>
+      </template>
+      <template v-slot:actions="{ callbacks }">
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="callbacks.close()"> Cancel </v-btn>
+          <v-btn color="green darken-1" text @click="callbacks.close()"> Insert </v-btn>
+      </template>
+    </Dialog>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            dialog: false
-        };
-    }
-};
+  import Dialog from "@/components/Dialog.vue"
+  export default {
+      components: {
+        Dialog
+      },
+      data() {
+          return {
+              dialog: false
+          };
+      },
+      methods: {
+        onSave() {
+          console.log("Saved data for Edit Device")
+        }
+      }
+  };
 </script>
-
-<style lang="scss" scoped>
-</style>
