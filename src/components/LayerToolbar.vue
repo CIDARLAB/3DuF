db2d0cb3c17de5ca74116a34958ea09f8f759f4a
Sherman Rose <sherman@shermanrose.uk>
2021-01-27 20:05:41 +0000

    Refactored LayerToolbar

--- src/components/LayerToolbar.vue
+++ src/components/LayerToolbar.vue
@@ -1,48 +1,39 @@
 <template>
-    <div>
-        <md-toolbar class="md-title" md-elevation="0">Select Layer</md-toolbar>
+  <v-card elevation="0">
+    <v-card-title class="py-2">
+      <span>Layers</span>
+      <v-btn icon small fab color="primary" @click="addLayer"><v-icon>mdi-plus</v-icon></v-btn>
+    </v-card-title>
 
-        <nav class="mdl-navigation" id="layer-controls">
-            <!--<a id="control_button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button&#45;&#45;raised view-button">Control</a>-->
+    <v-card-text class="px-1">
+        <div v-for="layer in layers" :key="layer.id" class="my-1 mx-3">
+            <v-btn icon small @click="deleteLayer(layer)">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
 
-            <div id="layer-toolbar">
-                <div
-                    id="template-layer-block"
-                    class="mdl-button-group layer-block"
-                    data-levelindex="0"
-                >
-                    <span class="level-index">LEVEL 1</span>
-                    <button
-                        class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored delete-level"
-                        style="visibility: hidden"
-                        data-levelindex="0"
-                    >
-                        <i class="material-icons">delete</i>
-                    </button>
-                    <!--<button class="mdl-button mdl-js-button mdl-button&#45;&#45;icon mdl-button&#45;&#45;colored">-->
-                    <!--<i class="material-icons">visibility_off</i>-->
-                    <!--</button>-->
-                    <br />
-                    <a
-                        class="mdl-button mdl-button--raised mdl-js-button mdl-js-ripple-effect layer-button flow-button"
-                        data-layerindex="0"
-                    >Flow</a>
-                    <a
-                        class="mdl-button mdl-button--raised mdl-js-button mdl-js-ripple-effect layer-button control-button"
-                        data-layerindex="1"
-                    >Control</a>
-                </div>
-            </div>
-
-            <button
-                id="add-new-level"
-                class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"
+            <v-btn-toggle
+              v-model="layer.mode"
+              mandatory
+              tile
             >
-                <i class="material-icons">add_box</i>
-            </button>
-            <!--<a id="cells_button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button&#45;&#45;raised view-button">Cells</a>-->
-        </nav>
-    </div>
+
+              <v-btn small 
+                :color="getButtonColor(layer, 0)"
+                @click="layerModeClicked(layer, 0)"
+              >
+                <span>Flow</span>
+              </v-btn>
+
+              <v-btn small 
+                :color="getButtonColor(layer, 1)"
+                @click="layerModeClicked(layer, 1)"
+              >
+                <span>Control</span>
+              </v-btn>
+            </v-btn-toggle>
+          </div>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -50,8 +41,43 @@ export default {
     name: "ComponentToolbar",
     data() {
         return {
-            test: "Level -"
+          layers: [],
+          selectedLayer: 0,
+          selectedMode: 0,
+          disabled: false
         };
+    },
+    mounted() {
+      this.addLayer()
+    },
+    methods: {
+      addLayer(){
+        let layer =  {
+          id: this.layers.length,
+          mode: 0
+        }
+        this.selectedLayer = layer.id
+        this.layers.push(layer)
+      },
+
+      layerModeClicked(layer) {
+        this.selectedLayer = layer.id
+      },
+
+      deleteLayer(layer) {
+        let idx = this.layers.findIndex(l => l.id == layer.id)
+        if(idx > -1) this.layers.splice(idx, 1)
+      },
+
+      getButtonColor(layer, buttonMode) {
+        if(layer.id != this.selectedLayer) return ""
+        if(layer.id == this.selectedLayer && layer.mode == buttonMode){
+          if(buttonMode == 0) return "blue white--text"
+          else return "red white--text"
+        }
+       return "" 
+      }
+
     }
 };
 </script>
