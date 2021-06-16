8b69d179ba2543fad85e8ab43fbb2caa4ad36be4
Liam Murray <liammurray@Liams-MacBook-Pro.local>
2021-06-16 05:44:59 -0400

    Added/modified registry.ts

--- src/app/core/registry.ts
+++ src/app/core/registry.ts
@@ -5,14 +5,11 @@ class Registry {
     Place where we store the data necessary for the text label
     TODO: Change this from this awful hacky implementation
   */
-    text = "";
 
     //TODO: Convert this into multiple text layers for use with multiple layers
-    textLayer = null;
     id_counter = 0;
     threeRenderer = null;
 
-    registeredParams: {[index: string]: any} = {};
     featureDefaults = {};
     currentDevice = null;
     canvasManager = null;
