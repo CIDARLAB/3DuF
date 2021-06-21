0f24fd209d175fa562b0e56ac048933322780dd4
Liam Murray <liammurray@Liams-MacBook-Pro.local>
2021-06-21 12:10:56 -0400

    Added String type allowance

--- src/app/core/parameters/stringValue.ts
+++ src/app/core/parameters/stringValue.ts
@@ -4,12 +4,12 @@ export default class StringValue extends Parameter {
     static readonly typeString: string = "String";
     static readonly description: string = "StringValue must be a String, such as 'foobar'";
 
-    constructor(value: string) {
+    constructor(value: string | String) {
         super("String",value);
     }
 
     static isValid(value: any) {
-        if (typeof value === "string") return true;
+        if (typeof value === "string" || value instanceof String) return true;
         else return false;
     }
 
