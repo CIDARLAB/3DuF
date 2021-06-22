c1a60c52550a67e36f2a291de7e3e0615c6d771b
Liam Murray <liammurray@Liams-MacBook-Pro.local>
2021-06-21 21:51:31 -0400

    Removed circular dependency

--- src/app/core/parameter.ts
+++ src/app/core/parameter.ts
@@ -1,14 +1,6 @@
 import Registry from './registry';
 import * as NumberUtils from "../utils/numberUtils";
 
-import StringValue from './parameters/stringValue';
-import FloatValue from './parameters/floatValue';
-import SegmentArray from './parameters/segmentArray';
-import PointArray from './parameters/pointArray';
-import PointValue from './parameters/pointValue';
-import IntegerValue from './parameters/integerValue';
-import BooleanValue from './parameters/booleanValue';
-
 
 /**
  * Parameter class
@@ -70,82 +62,4 @@ export default class Parameter {
     }
     
     resetValue(): void {}
-
-    /**
-     * Creates a parameter from a JSON format
-     * @param {JSON} json JSON format file with the parameters loaded
-     * @returns {Parameter} Returns a new parameter
-     * @memberof Parameter
-     */
-    static fromJSON(json: {type:string, value:any}): Parameter {
-        if (json.type == "String") {
-            return new StringValue(json.value);
-        } else if (json.type == "Float") {
-            return new FloatValue(json.value);
-        } else if (json.type == "Integer") {
-            return new IntegerValue(json.value);
-        } else if (json.type == "Point") {
-            return new PointValue(json.value);
-        } else if (json.type == "PointArray") {
-            return new PointArray(json.value);
-        } else if (json.type == "SegmentArray") {
-            return new SegmentArray(json.value);
-        } else if (json.type == "Boolean") {
-            return new BooleanValue(json.value);
-        } else {
-            throw new Error("json contains invalid type");
-        }
-
-    }
-    /**
-     * Generates a new parameter with a specific component
-     * @param {String} key Identifier of the parameter
-     * @param {*} value Value of the parameter
-     * @returns {Parameter} Returns a new parameter 
-     * @memberof Parameter
-     */
-    static generateComponentParameter(key: string, value: any): Parameter | Parameter[] {
-        let ret;
-
-        if (key == "position") {
-            ret = new PointValue(value);
-        } else if (NumberUtils.isFloatOrInt(value)) {
-            ret = new FloatValue(value);
-        } else if (typeof value == "string" || value instanceof String) {
-            ret = new StringValue(value);
-        } else {
-            throw new Error("Non-component passed to generateComponentParameter");
-        }
-
-        return ret;
-    }
-    /**
-     * Parameter for the connection object?
-     * @param {String} key Identifier of the parameter
-     * @param {*} value Value of the parameters
-     * @returns {Parameter} Returns a parameter object
-     * @memberof Parameter
-     */
-    static generateConnectionParameter(key: string, value: any): Parameter | Parameter[] {
-        let ret;
-
-        if (key == "paths") {
-            ret = [];
-            let point;
-            for (let i in value) {
-                point = value[i];
-                ret.push(new PointValue(point));
-            }
-        } else if (key == "segments") {
-            ret = new SegmentArray(value);
-        } else if (NumberUtils.isFloatOrInt(value)) {
-            ret = new FloatValue(value);
-        } else if (typeof value == "string" || value instanceof String) {
-            ret = new StringValue(value);
-        } else {
-            throw new Error("Non-connection passed to generateConnectionParameter");
-        }
-
-        return ret;
-    }
 }
