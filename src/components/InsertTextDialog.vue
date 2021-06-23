80eecb9d17cb86409b5d94089c7d5573d00d91f0
Leslie <lpu@berkeley.edu>
2021-06-22 17:54:05 -0700

    Insert Text Dialog

--- src/components/InsertTextDialog.vue
+++ src/components/InsertTextDialog.vue
@@ -1,36 +1,72 @@
 <template>
-    <Dialog title="Insert Text">
+    <Dialog title="Insert Text"
+        ><v-card-title class="h6">{{ Insert }}</v-card-title>
         <template #content>
             <form action="#">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input id="inserttext_textinput" class="mdl-textfield__input" type="text" />
-                    <label class="mdl-textfield__label" for="inserttext_textinput">Text</label>
-                </div>
+                <tr>
+                    <td>
+                        <v-card-text><p class="text--primary subtitle-1">Text:</p></v-card-text>
+                    </td>
+                    <td width="125px">
+                        <v-text-field></v-text-field>
+                    </td>
+                    <td>
+                        <v-card-text><p class="text--primary subtitle-1">Font Size:</p></v-card-text>
+                    </td>
+                    <td width="125px">
+                        <v-text-field v-model="number" :step="1" type="number"></v-text-field>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="100px">
+                        <v-card-text></v-card-text>
+                    </td>
+                    <td>
+                        <v-card-text></v-card-text>
+                    </td>
+                    <td>
+                        <v-card-text><p class="text--primary subtitle-1">Select Layer:</p></v-card-text>
+                    </td>
+                    <td>
+                        <v-menu offset-y>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                                    Dropdown
+                                </v-btn>
+                            </template>
+                            <v-list>
+                                <v-list-item v-for="(item, index) in items" :key="index">
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                        </v-menu>
+                    </td>
+                </tr>
             </form>
         </template>
         <template v-slot:actions="{ callbacks }">
             <v-spacer />
-            <v-btn color="green darken-1" text @click="callbacks.close()">
-                Cancel
-            </v-btn>
-            <v-btn color="green darken-1" text @click="callbacks.close()">
-                Insert
+            <v-col>
+                <v-btn class="pa-2" color="red darken-1" dark :style="{ right: '75%', transform: 'translateX(-100%)' }" @click="callbacks.close()">
+                    Cancel
+                </v-btn>
+                <v-spacer />
+            </v-col>
+            <v-btn class="pa-2" color="green darken-1" dark :style="{ right: '30%', transform: 'translateX(-30%)' }" @click="callbacks.close()">
+                Save
             </v-btn>
         </template>
     </Dialog>
 </template>
-
 <script>
 import Dialog from "@/components/base/Dialog.vue";
 export default {
     components: {
         Dialog
     },
-    data() {
-        return {
-            dialog: false
-        };
-    },
+    data: () => ({
+        items: [{ title: "Level1- FLOW" }, { title: "Level1- CONTROL" }, { title: "Level1- INTEGRATION" }]
+    }),
     methods: {
         onSave() {
             console.log("Saved data for Edit Device");
@@ -38,3 +74,6 @@ export default {
     }
 };
 </script>
+
+
+
