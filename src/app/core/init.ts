020c12733cdd959941acac05bd0ea7d587dfff11
Liam Murray <liammurray@Liams-MacBook-Pro.local>
2021-06-25 04:32:12 -0400

    Added 'Property'InterchangeV1 definitions

---
+++ src/app/core/init.ts
@@ -0,0 +1,60 @@
+import StringValue from "./parameters/stringValue"
+import Component from "./component"
+import Connection from "./connection"
+import Feature from "./feature"
+
+type Point = [number, number]
+
+type Segment = number[]
+
+export type DeviceInterchangeV1 = {
+    name: StringValue
+    params: any
+    //layers: any[]
+    groups: Array<string>
+    components: Array<ComponentInterchangeV1>
+    connections: Array<ConnectionInterchangeV1>
+    features: Array<LayerInterchangeV1>
+    version: number
+}
+
+export type ComponentInterchangeV1 = {
+    id: string
+    name: string 
+    entity: string
+    params: any
+    xspan: number
+    yspan: number
+    ports: any[]
+    layer: Array<string>
+}
+
+export type ConnectionInterchangeV1 = {
+    id: string
+    name: string
+    entity: string
+    source: any
+    sinks: any
+    paths: [number, number][]
+    params: any
+    layer: string
+}
+
+export type LayerInterchangeV1 = {
+    id: string
+    name: string
+    params: any
+    group: string
+    type: string
+}
+
+export type FeatureInterchangeV1 = {
+    id: string
+    name: string
+    macro: string
+    set: any
+    referenceID: string
+    params: any
+    dxfData: any
+    type: string
+}
