9eb26560c306395dd30b1d574ab4f650dcf1cc9e
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2021-07-08 18:49:13 -0400

    Removed all the warnings

--- src/components/ComponentToolBar.vue
+++ src/components/ComponentToolBar.vue
@@ -7,7 +7,7 @@
                 <span>Feature</span>
             </v-card-title>
             <v-card-text class="px-1">
-                <ConnectionPropertyDrawer title="Connection" />
+                <ConnectionPropertyDrawer title="Connection" :spec="connectionSpec" />
                 <PropertyDrawer title="Channel" :spec="channelSpec" />
                 <PropertyDrawer title="Rounded Channel" :spec="roundedChannelSpec" />
                 <PropertyDrawer title="Transition" :spec="transitionSpec" />
