61b9db4d4e48cf884b9d706557e9467d454a567e
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2021-07-22 00:01:31 -0400

    Readded the insert text dialog

--- src/components/InsertTextDialog.vue
+++ src/components/InsertTextDialog.vue
@@ -1,42 +1,72 @@
 <template>
-    <Dialog title="Insert Text">
+    <Dialog title="Insert Text"
+        ><v-card-title class="h6">Insert</v-card-title>
         <template #content>
             <form action="#">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input id="inserttext_textinput" class="mdl-textfield__input" type="text" />
-                    <label class="mdl-textfield__label" for="inserttext_textinput">Text</label>
-                </div>
+                <tr>
+                    <td>
+                        <v-card-text><p class="text--primary subtitle-1">Text:</p></v-card-text>
+                    </td>
+                    <td width="125px">
+                        <v-text-field :v-model="text"></v-text-field>
+                    </td>
+                    <td>
+                        <v-card-text><p class="text--primary subtitle-1">Font Size:</p></v-card-text>
+                    </td>
+                    <td width="125px">
+                        <v-text-field v-model="fontSize" :step="1" type="number"></v-text-field>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="100px">
+                        <v-card-text></v-card-text>
+                    </td>
+                    <td>
+                        <v-card-text></v-card-text>
+                    </td>
+                    <td>
+                        <v-card-text><p class="text--primary subtitle-1">Select Layer:</p></v-card-text>
+                    </td>
+                    <td>
+                        <v-select :items="items" label="Dropdown"> </v-select>
+                    </td>
+                </tr>
             </form>
         </template>
         <template v-slot:actions="{ callbacks }">
             <v-spacer />
-            <v-btn color="green darken-1" text @click="callbacks.close()"> Cancel </v-btn>
-            <v-btn color="green darken-1" text @click="callbacks.close()"> Insert </v-btn>
+            <v-col>
+                <v-btn class="pa-2" color="red darken-1" dark :style="{ right: '75%', transform: 'translateX(-100%)' }" @click="callbacks.close()">
+                    Cancel
+                </v-btn>
+                <v-spacer />
+            </v-col>
+            <v-btn class="pa-2" color="green darken-1" dark :style="{ right: '30%', transform: 'translateX(-30%)' }" @click="onClick">
+                Insert
+            </v-btn>
         </template>
     </Dialog>
 </template>
-
 <script>
 import Dialog from "@/components/base/Dialog.vue";
 import Registry from "@/app/core/registry.ts";
-import insertTextTool from "@/app/view/tools/insertTextTool.js";
 export default {
     components: {
         Dialog
     },
-    data() {
-        return {
-            dialog: false
-        };
-    },
+    data: () => ({
+        items: ["Level1- FLOW", "Level1- CONTROL", "Level1- INTEGRATION"],
+        text: "",
+        fontSize: 12
+    }),
     methods: {
         onSave() {
             console.log("Saved data for Edit Device");
         },
-        onclick() {
+        onClick() {
             Registry.viewManager.activateTool("InsertTextTool");
-            insertTextTool.text = document.getElementById("inserttext_textinput").value;
-            this.__dialog.close();
+            Registry.viewManager.tools.InsertTextTool.text = this.text;
+            this.close();
         }
     }
 };
