1505ebee566c4ec9f45f9e52452927c14184fe90
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2021-08-12 23:14:08 -0400

    WIP

--- src/components/ImportDXFDialog.vue
+++ src/components/ImportDXFDialog.vue
@@ -1,19 +1,22 @@
 <template>
     <Dialog title="Import" color="primary" text-color="white--text">
         <template #content>
+            <h4>Drag Drop the DXF Border File:</h4>
             <div class="mdl-dialog__content">
-                <canvas id="component_preview_canvas" tabindex="1" width="500" height="400" />
+                <canvas id="component_preview_canvas" tabindex="1" width="400" height="200" color="gray" />
+                <input id="dxf_input" ref="file" type="file" class="upload" />
             </div>
-            <form action="#">
+            <!-- <form action="#">
                 <div class="mdl-textfield mdl-js-textfield" width="300px">
                     <input id="new_component_name" class="mdl-textfield__input" type="text" />
-                    <label class="mdl-textfield__label" for="new_component_name">Name...</label>
+                    <label class="mdl-textfield__label" for="new_component_name">Name......</label>
                 </div>
-            </form>
+            </form> -->
         </template>
         <template #actions="{ callbacks }">
-            <v-btn color="green darken-1" text @click="callbacks.close()"> Cancel </v-btn>
-            <v-btn color="green darken-1" text @click="callbacks.close()"> Import </v-btn>
+            <v-btn class="white--text" color="green dark" @click="callbacks.close()"> Import </v-btn>
+            <v-btn class="white--text" color="red dark" @click="callbacks.close()"> Cancel </v-btn>
+            <v-btn color="white" @click="callbacks.close(onSave)"> Okay </v-btn>
         </template>
     </Dialog>
 </template>
@@ -36,5 +39,4 @@ export default {
     }
 };
 </script>
-
 <style lang="scss" scoped></style>
