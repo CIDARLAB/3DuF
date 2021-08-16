71fa05d35002f83931ff1da23b3fa66061ecab00
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2021-08-15 22:12:49 -0400

    fixed canvas no show issue

--- src/components/ImportDXFDialog.vue
+++ src/components/ImportDXFDialog.vue
@@ -1,22 +1,15 @@
 <template>
     <Dialog title="Import" color="primary" text-color="white--text">
         <template #content>
-            <h4>Drag Drop the DXF Border File:</h4>
+            <h4>Drag and Drop the DXF File:</h4>
             <div class="mdl-dialog__content">
-                <canvas id="component_preview_canvas" tabindex="1" width="400" height="200" color="gray" />
+                <v-card id="drop_box" class="mx-auto" tile outlined width="400" color="grey lighten-2" height="200"> </v-card>
                 <input id="dxf_input" ref="file" type="file" class="upload" />
             </div>
-            <!-- <form action="#">
-                <div class="mdl-textfield mdl-js-textfield" width="300px">
-                    <input id="new_component_name" class="mdl-textfield__input" type="text" />
-                    <label class="mdl-textfield__label" for="new_component_name">Name......</label>
-                </div>
-            </form> -->
         </template>
         <template #actions="{ callbacks }">
             <v-btn class="white--text" color="green dark" @click="callbacks.close()"> Import </v-btn>
             <v-btn class="white--text" color="red dark" @click="callbacks.close()"> Cancel </v-btn>
-            <v-btn color="white" @click="callbacks.close(onSave)"> Okay </v-btn>
         </template>
     </Dialog>
 </template>
@@ -39,4 +32,13 @@ export default {
     }
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#drop_box {
+    position: relative;
+    top: 10px;
+}
+#dxf_input {
+    position: relative;
+    top: 30px;
+}
+</style>
