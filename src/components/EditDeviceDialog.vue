6a3c1e1abbcadf14abb0129c793e78dd1ef3ff7a
Roy Chen <rxchen123@gmail.com>
2021-08-18 08:18:26 -0400

    EditDeviceDialog update 1.0

--- src/components/EditDeviceDialog.vue
+++ src/components/EditDeviceDialog.vue
@@ -1,31 +1,40 @@
 <template>
     <Dialog title="Edit Device">
         <template v-slot:content>
-            <h4>Rename:</h4>
-            <form action="#">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input id="devicename_textinput" class="mdl-textfield__input" type="text" />
-                    <label class="mdl-textfield__label" for="devicename_textinput">Device Name</label>
-                </div>
-            </form>
-
-            <h4>Resize:</h4>
-            <form action="#">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input id="xspan_textinput" class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" />
-                    <label class="mdl-textfield__label" for="xspan_textinput">X-Span (mm)</label>
-                    <span class="mdl-textfield__error">Input is not a number!</span>
-                </div>
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input id="yspan_textinput" class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" />
-                    <label class="mdl-textfield__label" for="yspan_textinput">Y-Span (mm)</label>
-                    <span class="mdl-textfield__error">Input is not a number!</span>
-                </div>
-            </form>
+            <tr>
+                <h4 class="text--primary subtitle-1">Rename:</h4>
+                <td>
+                    <form action="#">
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <tr>
+                                <v-text-field id="devicename_textinput" class="mdl-textfield__input" type="text" />
+                                <label class="mdl-textfield__label" for="devicename_textinput">Device Name</label>
+                            </tr>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+            <tr>
+                <h4 class="text--primary subtitle-1">Resize:</h4>
+                <td>
+                    <form action="#">
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <v-text-field id="xspan_textinput" class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" />
+                            <label class="mdl-textfield__label" for="xspan_textinput">X-Span (mm)</label>
+                            <!-- <span class="mdl-textfield__error">Input is not a number!</span> -->
+                        </div>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <v-text-field id="yspan_textinput" class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" />
+                            <label class="mdl-textfield__label" for="yspan_textinput">Y-Span (mm)</label>
+                            <!-- <span class="mdl-textfield__error">Input is not a number!</span> -->
+                        </div>
+                    </form>
+                </td>
+            </tr>
         </template>
         <template v-slot:actions="{ callbacks }">
-            <v-btn color="green darken-1" text @click="callbacks.close()"> Cancel </v-btn>
-            <v-btn color="green darken-1" text @click="callbacks.close(onSave)"> Save </v-btn>
+            <v-btn dark color="green dark" elevation="2" @click="callbacks.close(onSave)"> Save </v-btn>
+            <v-btn dark color="red dark" elevation="2" @click="callbacks.close()"> Cancel </v-btn>
         </template>
     </Dialog>
 </template>
