ef329c93cd882ade1e9fbc691fb4706d0436ac10
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2022-07-14 00:17:49 -0400

    Fixed formatting

--- src/primitives-server/src/routes/componentAPI.ts
+++ src/primitives-server/src/routes/componentAPI.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import controller from "../controllers/componentAPI";
+import controller from "../controllers/primitiveController";
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Welcome to the Component API",
-  });
+    return res.status(200).json({
+        message: "Welcome to the Component API"
+    });
 });
 router.get("/dimensions", controller.getDimensions);
 router.get("/terminals", controller.getTerminals);
--- src/primitives-server/src/server.ts
+++ src/primitives-server/src/server.ts
@@ -15,19 +15,16 @@ router.use(express.json());
 
 /** RULES OF OUR API */
 router.use((req, res, next) => {
-  // set the CORS policy
-  res.header("Access-Control-Allow-Origin", "*");
-  // set the CORS headers
-  res.header(
-    "Access-Control-Allow-Headers",
-    "origin, X-Requested-With,Content-Type,Accept, Authorization"
-  );
-  // set the CORS method headers
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
-    return res.status(200).json({});
-  }
-  next();
+    // set the CORS policy
+    res.header("Access-Control-Allow-Origin", "*");
+    // set the CORS headers
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With,Content-Type,Accept, Authorization");
+    // set the CORS method headers
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+        return res.status(200).json({});
+    }
+    next();
 });
 
 /** Routes */
@@ -35,15 +32,13 @@ router.use("/", routes);
 
 /** Error handling */
 router.use((req, res, next) => {
-  const error = new Error("not found");
-  return res.status(404).json({
-    message: error.message,
-  });
+    const error = new Error("not found");
+    return res.status(404).json({
+        message: error.message
+    });
 });
 
 /** Server */
 const httpServer = http.createServer(router);
 const PORT: any = process.env.PORT ?? 6060;
-httpServer.listen(PORT, () =>
-  console.log(`The server is running on port ${PORT}`)
-);
+httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
