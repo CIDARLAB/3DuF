3d301a0586a044a521fb2e157f4bce1e4ee8ae15
woo <hanwj100@gmail.com>
2024-09-04 19:54:33 -0400

    Deploy Build to GH Pages

--- src/plugins/vuetify.ts
+++
@@ -1,7 +0,0 @@
-import Vue from "vue";
-import Vuetify from "vuetify";
-import "@mdi/font/css/materialdesignicons.css";
-
-Vue.use(Vuetify);
-
-export default new Vuetify({});
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    transpileDependencies: ["vuetify"],
-    configureWebpack: {
-        devtool: "source-map"
-    },
-    publicPath: "/"
-};
