d1bf977e1d52ea364d3f744851608edf4dd8332b
Eric Xie <ericx@bu.edu>
2025-03-10 15:08:10 -0400

    fixed merge

--- src/app/library/blackBox.ts
+++ src/app/library/blackBox.ts
@@ -18,13 +18,10 @@ export default class BlackBox extends Template {
             width: "Float",
             length: "Float",
             height: "Float",
-<<<<<<< HEAD
-=======
             cornerRadius: "Float",
             rotation: "Float",
             mirrorByX: "Float",
             mirrorByY: "Float"
->>>>>>> webpack-build
         };
 
         this.__defaults = {
@@ -32,13 +29,10 @@ export default class BlackBox extends Template {
             width: 5000,
             length: 5000,
             height: 250,
-<<<<<<< HEAD
-=======
             cornerRadius: 200,
             rotation: 0,
             mirrorByX: 0,
             mirrorByY: 0
->>>>>>> webpack-build
         };
 
         this.__units = {
@@ -46,10 +40,7 @@ export default class BlackBox extends Template {
             width: "μm",
             length: "μm",
             height: "μm",
-<<<<<<< HEAD
-=======
             cornerRadius: "μm",
->>>>>>> webpack-build
             rotation: "°"
         };
 
@@ -58,13 +49,10 @@ export default class BlackBox extends Template {
             width: 5,
             length: 5,
             height: 1,
-<<<<<<< HEAD
-=======
             cornerRadius: 1,
             rotation: 0,
             mirrorByX: 0,
             mirrorByY: 0
->>>>>>> webpack-build
         };
 
         this.__maximum = {
@@ -72,13 +60,10 @@ export default class BlackBox extends Template {
             width: 50000,
             length: 50000,
             height: 50000,
-<<<<<<< HEAD
-=======
             cornerRadius: 1000,
             rotation: 360,
             mirrorByX: 1,
             mirrorByY: 1
->>>>>>> webpack-build
         };
 
         this.__featureParams = {
@@ -87,13 +72,10 @@ export default class BlackBox extends Template {
             width: "width",
             length: "length",
             height: "height",
-<<<<<<< HEAD
-=======
             cornerRadius: "cornerRadius",
             rotation: "rotation",
             mirrorByX: "mirrorByX",
             mirrorByY: "mirrorByY"
->>>>>>> webpack-build
         };
 
         this.__targetParams = {
@@ -102,13 +84,10 @@ export default class BlackBox extends Template {
             width: "width",
             length: "length",
             height: "height",
-<<<<<<< HEAD
-=======
             cornerRadius: "cornerRadius",
             rotation: "rotation",
             mirrorByX: "mirrorByX",
             mirrorByY: "mirrorByY"
->>>>>>> webpack-build
         };
 
         this.__placementTool = "componentPositionTool";
@@ -136,10 +115,6 @@ export default class BlackBox extends Template {
 
         const ports = [];
 
-<<<<<<< HEAD
-        ports.push(new ComponentPort(0, l/2, "1", LogicalLayerType.FLOW));
-        ports.push(new ComponentPort(w, l/2, "2", LogicalLayerType.FLOW));
-=======
         ports.push(new ComponentPort(0, -l / 2, "1", LogicalLayerType.FLOW));
 
         ports.push(new ComponentPort(w / 2, 0, "2", LogicalLayerType.FLOW));
@@ -147,7 +122,6 @@ export default class BlackBox extends Template {
         ports.push(new ComponentPort(0, l / 2, "3", LogicalLayerType.FLOW));
 
         ports.push(new ComponentPort(-w / 2, 0, "4", LogicalLayerType.FLOW));
->>>>>>> webpack-build
 
         return ports;
     }
@@ -158,21 +132,6 @@ export default class BlackBox extends Template {
         const py = position[1];
         const l = params.length;
         const w = params.width;
-<<<<<<< HEAD
-        const color = params.color;
-        
-        const rendered = new paper.CompoundPath("");
-
-        const rec = new paper.Path.Rectangle({
-            point: new paper.Point(px, py),
-            size: [w, l]
-        });
-
-        rendered.addChild(rec);
-
-        rendered.fillColor = color;
-
-=======
         const rotation = params.rotation;
         const color = params.color;
         const radius = params.cornerRadius;
@@ -195,7 +154,6 @@ export default class BlackBox extends Template {
 
         this.transformRender(params,rendered);
 
->>>>>>> webpack-build
         return rendered;
     }
 
@@ -207,8 +165,4 @@ export default class BlackBox extends Template {
         render.fillColor!.alpha = 0.5;
         return render;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> webpack-build
