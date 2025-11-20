9ec5d23ec8ac95e726cc844542a9668ca308a876
Woo Zhong Han <hanwj100@users.noreply.github.com>
2025-11-20 09:31:47 -0500

    Update gh-pages with webpack build

--- src/app/view/render3D/threeUtils.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three";
-
-export function mergeGeometries(geometries) {
-    var merged = new THREE.Geometry();
-    for (var i = 0; i < geometries.length; i++) {
-        merged.merge(geometries[i]);
-    }
-    return merged;
-}
--- src/app/view/tools/copyTool.js
+++
@@ -1,9 +0,0 @@
-// var MouseTool = require("./positionTool");
-// import * as  Registry from "../../core/registry";
-// var Feature = require("../../core/feature");
-// import SimpleQueue from "../../utils/simpleQueue";
-// var PageSetup = require("../pageSetup");
-
-export default class CopyTool extends positionTool {
-    // constructor(typeString, setString, )
-}
