29e3b24bd400041b80887b64f478b81d85d5b916
zyrrron <1043251725@qq.com>
2026-03-19 00:35:16 -0400

    update

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ module.exports = {
     transpileDependencies: ["vuetify"],
     configureWebpack: {
         devtool: "source-map"
+    },
+    devServer: {
+        port: 8082,
+        disableHostCheck: true,
+        proxy: {
+            '/api': { target: 'http://localhost:8080', changeOrigin: true },
+            '/socket.io': { target: 'http://localhost:3000', ws: true }
+        }
     }
 };
